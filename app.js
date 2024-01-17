e35822a1562316ae921a56d0de09301c145837df
Jerome Marrero <jerome.marrero.perez@gmail.com>
2024-01-17 13:59:48 -0400

    added colons

--- app.js
+++ app.js
@@ -4,30 +4,29 @@ function createInstructor(firstName, lastName){
       firstName: firstName,
       lastName: lastName
     }
-  }
+  };
 //newVersion
 function createInstructor2(firstName, lastName){
     return {
         firstName,
         lastName
     };
-}
+};
 
 // oldVersion
 var favoriteNumber = 42;
 
 var instructor = {
   firstName: "Colt"
-}
-
-instructor[favoriteNumber] = "That is my favorite!"
+};
+instructor[favoriteNumber] = "That is my favorite!";
 
 // newVersion
 let favoriteNumber
 const instructor = {
     firstName: "Colt",
     [favoriteNumber]: "that is my favorite!"
-}
+};
 
 // oldVersion 
 var instructor = {
@@ -38,7 +37,7 @@ var instructor = {
     sayBye: function(){
       return this.firstName + " says bye!";
     }
-  }
+  };
   /* Write an ES2015 Version */
   const instructor = {
     [firstName]: "Colt",
@@ -46,17 +45,17 @@ var instructor = {
         return "Hi!";
     },
     sayBye(){
-        return this.firstName + " says bye!"
+        return this.firstName + " says bye!";
 
     }
-  }
+  };
 // create Animal function 
 
 function createAnimal(species, verb, noise){
     return {
         species,
         [verb](){
-            return noise
-        };
-    }
-}
+            return noise;
+        }
+    };
+};
